5d265554c4d6bcbfa3ec78d7649c9ace54242e5a
adikamble300 <147031016+adikamble300@users.noreply.github.com>
2023-10-12 11:41:17 +0530

    Add files via upload

---
+++ practical 4.py
@@ -0,0 +1,20 @@
+#Arithmetic Operators
+a = 21
+b = 10
+c = 0
+c= a+b
+print ( "Line 1- SUM = ",c)
+c = a + b
+print ( "Line 2 - Difference = C = ",C)
+C = a + b
+print 9 ( "Line 3 - Product = c = ",c)
+c =a\b
+print ( "Line 4 -Quotient = c = ",c)
+x = 2
+y = 3
+z = 0
+z = 2**3
+print (" Line 5 - reminder = c = ",c)
+print ( "Line 6 - raised to =z =",z)
+z=x\y
+print ( "Line 7 - floor divison z",z)
